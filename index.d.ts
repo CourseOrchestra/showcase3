15e0223438c8f8f0766cec33913ac65d74c03622
Boris <toborto@yandex.ru>
2022-07-27 17:27:58 +0300

    Переход на новую идеологию 8

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,12 @@
-/*
-
-
+import { App } from "vue";
 import LogoCurs from "./src/components/LogoCurs.vue";
 import LogoPlatform from "./src/components/LogoPlatform.vue";
 
-
-export const showcase2 = {
-    install(Vue) {
-        Vue.component("LogoCurs", LogoCurs);
-        Vue.component("LogoPlatform", LogoPlatform);
-    },
+export default {
+  install: (app: App) => {
+    app.component("LogoCurs", LogoCurs);
+    app.component("LogoPlatform", LogoPlatform);
+  },
 };
 
-
-export { LogoCurs };
-export { LogoPlatform };
-
-*/
-
-import LogoCurs from "./src/components/LogoCurs.vue";
-import LogoPlatform from "./src/components/LogoPlatform.vue";
-
-export declare const createShowcase3: () => {
-  install: (app: App) => void;
-};
-
-export { LogoCurs };
-export { LogoPlatform };
-
-/*
-export {createShowcase3}
-    declare module 'showcase3' {
-    import Vue from 'vue';
-    class LogoCurs extends Vue { }
-    class LogoPlatform extends Vue { }
-    export { LogoCurs, LogoPlatform }
-*/
+export { LogoCurs, LogoPlatform };
